7168e7eb08d535593a63cf98913f831f4acca2f5
ZahraReyhanian <reyhanyanz@gmail.com>
2021-05-01 12:08:56 +0430

    create models and migrations

---
+++ database/migrations/2021_04_29_193555_create_panels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePanelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('panels', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('seller_id');
+            $table->foreign('seller_id')->on('sellers')->references('id')->onDelete('cascade');
+            $table->string('image');
+            $table->timestamp('birthday');
+            $table->string('job_title');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('panels');
+    }
+}
---
+++ database/migrations/2021_04_29_194348_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('seller_id');
+            $table->foreign('seller_id')->on('sellers')->references('id')->onDelete('cascade');
+            $table->text('images');
+            $table->text('description');
+            $table->string('slug');
+            $table->integer('view_count');
+            $table->integer('comment_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
